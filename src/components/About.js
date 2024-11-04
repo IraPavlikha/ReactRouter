8f717be248280d711f6b2f7c416e5a97c12a0d84
IrenaPavlikha <kotova.dream@gmail.com>
2024-11-04 19:26:01 +0200

    Initial commit

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+// src/components/About.js
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>Про нас</h1>
+            <p>Це простий веб-додаток на React з використанням React Router.</p>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+// src/components/Home.js
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Головна сторінка</h1>
+            <p>Ласкаво просимо на наш сайт!</p>
+        </div>
+    );
+};
+
+export default Home;
